9051cd9bce0ce3c6c32b093ad6303d3fd97c138b
Jorge Teixeira <jorge92@mailbox.org>
2019-07-30 16:27:13 +0100

    table sorting is now possible [issue 6]

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,32 +1,63 @@
 import React from 'react';
-
-import TableEntrie from './TableEntrie';
+import ReactTable from 'react-table';
 
 const Table = ({top10Videos}) => {
+
+  // const thumb = top10Videos.map(video => {
+  //   return video.thumbnail_link;
+  // }); 
+
+  const columns = [{
+    Header: 'Thumbnail',
+    acessor: 'thumbnail_link',
+    // Cell: (thumb) => {
+    //   return <img src={thumb.thumbnail_link}></img>
+    // }
+  },{
+    Header: 'Title',
+    accessor: 'title'
+  },{
+    Header: 'Channel',
+    accessor: 'channel_title'
+  },{
+    Header: 'Views',
+    accessor: 'views'
+  },{
+    Header: 'Likes',
+    accessor: 'likes'
+  },{
+    Header: 'Id',
+    accessor: 'video_id'
+  },
+  ];
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Thumbnail</th>
-          <th>Title</th>
-          <th>Channel</th>
-          <th>Likes</th>
-          <th>Views</th>
-        </tr>
-      </thead>
-      <tbody>
-        {top10Videos.map(video =>
-        < TableEntrie
-          key={video.id}
-          likes={video.likes}
-          title={video.title}
-          channel={video.channel_title}
-          views={video.views}
-          thumbnail_link={video.thumbnail_link}
-        >
-        </TableEntrie>)}
-      </tbody>
-    </table>
+    <div>
+      <ReactTable data={top10Videos} columns={columns} />
+    </div>
+    // <table>
+    //   <thead>
+    //     <tr>
+    //       <th>Thumbnail</th>
+    //       <th>Title</th>
+    //       <th>Channel</th>
+    //       <th>Likes</th>
+    //       <th>Views</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {top10Videos.map(video =>
+    //     < TableEntrie
+    //       key={video.id}
+    //       likes={video.likes}
+    //       title={video.title}
+    //       channel={video.channel_title}
+    //       views={video.views}
+    //       thumbnail_link={video.thumbnail_link}
+    //     >
+    //     </TableEntrie>)}
+    //   </tbody>
+    // </table>
   )
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import 'react-table/react-table.css';
+
 ReactDOM.render(<App />, document.getElementById('root'))
