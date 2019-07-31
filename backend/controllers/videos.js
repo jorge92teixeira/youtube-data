9cbf6ea70ab5d92681ae7c1af40f07ce4cdd40dc
Jorge Teixeira <jorge92@mailbox.org>
2019-07-31 13:26:46 +0100

    prevent tables from showing the same video [issue 38]

--- backend/controllers/videos.js
+++ backend/controllers/videos.js
@@ -13,8 +13,36 @@ const { frVideo } = require('../models/video');
 // });
 
 videosRouter.get('/top10likesgb', async (req, res) => {
-  const videos = await gbVideo.find({}).sort({ likes: -1 }).limit(10);
-  res.json(videos.map(video => video.toJSON()));
+
+  // const query = await gbVideo.aggregate([
+  //   {
+  //     $sort: { likes: -1 },
+  //   },
+  //   {
+  //     $limit: 100,
+  //   },
+  //   {
+  //     $group: { _id: null, values: { $addToSet: '$video_id' } },
+  //   },
+  // ]);
+  // console.log(query[0].values.length);
+
+  // const videos = await gbVideo.find({
+  //   'video_id': { $in: query[0].values },
+  // });
+  // console.log(videos.length);
+
+  // console.log(videos[0].values.slice(0, 10));
+
+  const videos = await gbVideo.find({}).sort({ likes: -1 }).limit(1000);
+  let result = videos.map(e => e.video_id)
+    .map((e, i, final) => final.indexOf(e) === i && i)
+    .filter(e => videos[e])
+    .map(e => videos[e]);
+
+  result = result.slice(0, 10);
+
+  res.json(result.map(video => video.toJSON()));
 });
 videosRouter.get('/top10likesca', async (req, res) => {
   const videos = await caVideo.find({}).sort({ likes: -1 }).limit(10);
